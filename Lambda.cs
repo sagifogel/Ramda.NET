798dd6df563c1b0b8d5600ea7636eab97d80f067
Sagi <sagifogel@hotmail.com>
2016-10-30 01:47:46 +0200

    removed unused delegate type

--- Lambda.cs
+++ Lambda.cs
@@ -9,6 +9,5 @@ namespace Ramda.NET
         public delegate dynamic Lambda2(object arg1 = null, object arg2 = null);
         public delegate dynamic Lambda3(object arg1 = null, object arg2 = null, object arg3 = null);
         public delegate dynamic LambdaN(object arg1 = null, object arg2 = null, object arg3 = null, object arg4 = null, object arg5 = null, object arg6 = null, object arg7 = null, object arg8 = null, object arg9 = null, object arg10 = null);
-        public delegate Task LambdaTaskN(object arg1 = null, object arg2 = null, object arg3 = null, object arg4 = null, object arg5 = null, object arg6 = null, object arg7 = null, object arg8 = null, object arg9 = null, object arg10 = null);
     }
 }

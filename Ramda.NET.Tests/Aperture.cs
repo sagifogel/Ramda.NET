7117ea9cdfdc19a5a85e07598d41b794ce7d0f9c
Sagi <sagifogel@hotmail.com>
2016-11-17 00:41:25 +0200

    addition of Aperture unit-test

---
+++ Ramda.NET.Tests/Aperture.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace Ramda.NET.Tests
+{
+    [TestClass]
+    public class Aperture
+    {
+        private int[] sevenLs = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+        [TestMethod]
+        public void Aperture_Creates_A_List_Of_N_tuples_From_A_List() {
+            CollectionAsserNestedArray((Array)R.Aperture(1, sevenLs), new[] { new[] { 1 }, new[] { 2 }, new[] { 3 }, new[] { 4 }, new[] { 5 }, new[] { 6 }, new[] { 7 } });
+            CollectionAsserNestedArray((Array)R.Aperture(2, sevenLs), new[] { new[] { 1, 2 }, new[] { 2, 3 }, new[] { 3, 4 }, new[] { 4, 5 }, new[] { 5, 6 }, new[] { 6, 7 } });
+            CollectionAsserNestedArray((Array)R.Aperture(3, sevenLs), new[] { new[] { 1, 2, 3 }, new[] { 2, 3, 4 }, new[] { 3, 4, 5 }, new[] { 4, 5, 6 }, new[] { 5, 6, 7 } });
+            CollectionAsserNestedArray((Array)R.Aperture(4, new[] { 1, 2, 3, 4 }), new[] { new[] { 1, 2, 3, 4 } });
+        }
+
+        [TestMethod]
+        [Description("Aperture_Returns_An_Empty_List_When_`n`_>_`list.length")]
+        public void Aperture_Returns_An_Empty_List_When_N_Greater_Then_List_Length() {
+            CollectionAsserNestedArray((Array)R.Aperture(1, sevenLs), new[] { new[] { 1 }, new[] { 2 }, new[] { 3 }, new[] { 4 }, new[] { 5 }, new[] { 6 }, new[] { 7 } });
+            CollectionAsserNestedArray((Array)R.Aperture(2, sevenLs), new[] { new[] { 1, 2 }, new[] { 2, 3 }, new[] { 3, 4 }, new[] { 4, 5 }, new[] { 5, 6 }, new[] { 6, 7 } });
+            CollectionAsserNestedArray((Array)R.Aperture(3, sevenLs), new[] { new[] { 1, 2, 3 }, new[] { 2, 3, 4 }, new[] { 3, 4, 5 }, new[] { 4, 5, 6 }, new[] { 5, 6, 7 } });
+            CollectionAsserNestedArray((Array)R.Aperture(4, new[] { 1, 2, 3, 4 }), new[] { new[] { 1, 2, 3, 4 } });
+        }
+
+
+        [TestMethod]
+        [Description("Aperture_Returns_An_Empty_List_When_`n`_>_`list.length")]
+        public void Aperture_Is_Curried() {
+            var pairwise = R.Aperture(2);
+            var res = (Array)pairwise(sevenLs);
+            var expected = new[] { new[] { 1, 2 }, new[] { 2, 3 }, new[] { 3, 4 }, new[] { 4, 5 }, new[] { 5, 6 }, new[] { 6, 7 } };
+
+            CollectionAsserNestedArray(res, expected);
+        }
+
+        [TestMethod]
+        public void Aperture_Can_Act_As_A_Transducer() {
+            var expected = new[] { new[] { 1, 2 }, new[] { 2, 3 }, new[] { 3, 4 }, new[] { 4, 5 }, new[] { 5, 6 }, new[] { 6, 7 } };
+            var res = (Array)R.Into(new object[0], R.Aperture(2), sevenLs);
+
+            CollectionAsserNestedArray(res, expected);
+        }
+
+        private void CollectionAsserNestedArray(Array result, IList expected) {
+            result.ForEach((item, i) => {
+                var innerArray = expected[i];
+
+                CollectionAssert.AreEqual((ICollection)item, (ICollection)innerArray);
+            });
+        }
+    }
+}

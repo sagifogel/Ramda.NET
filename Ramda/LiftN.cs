37098d5c444399b7bce8f23f14d81c73b5045bda
Sagi <sagifogel@hotmail.com>
2016-12-10 23:17:59 +0200

    wrapping dynamic types with DynamicDelegate

--- Ramda/LiftN.cs
+++ Ramda/LiftN.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Dynamic;
 using System.Collections;
+using static Ramda.NET.Currying;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
@@ -14,11 +15,11 @@ namespace Ramda.NET
 	public static partial class R
 	{	
 		public static dynamic LiftN(int n, Delegate fn) {
-			return Currying.LiftN(n, new DelegateDecorator(fn));
+			return Currying.LiftN(n, Delegate(fn));
 		}
 
 		public static dynamic LiftN(RamdaPlaceholder n, Delegate fn) {
-			return Currying.LiftN(n, new DelegateDecorator(fn));
+			return Currying.LiftN(n, Delegate(fn));
 		}
 
 		public static dynamic LiftN(int n, RamdaPlaceholder fn = null) {

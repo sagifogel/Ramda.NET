4dce5509635dc8384ee6a8d616ffb1a899305aab
Sagi <sagifogel@hotmail.com>
2016-12-10 23:16:09 +0200

    wrapping dynamic types with DynamicDelegate

--- Ramda/Map.cs
+++ Ramda/Map.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Dynamic;
 using System.Collections;
+using static Ramda.NET.Currying;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
@@ -14,7 +15,7 @@ namespace Ramda.NET
 	public static partial class R
 	{	
 		public static dynamic Map<TSource, TReturn>(Func<TSource, TReturn> fn, IList<TSource> list) {
-			return Currying.Map(new DelegateDecorator(fn), list);
+			return Currying.Map(Delegate(fn), list);
 		}
 
 		public static dynamic Map<TSource>(RamdaPlaceholder fn, IList<TSource> list) {
@@ -22,7 +23,7 @@ namespace Ramda.NET
 		}
 
 		public static dynamic Map<TSource, TReturn>(Func<TSource, TReturn> fn, RamdaPlaceholder list = null) {
-			return Currying.Map(new DelegateDecorator(fn), list);
+			return Currying.Map(Delegate(fn), list);
 		}
 
 		public static dynamic Map(dynamic fn, RamdaPlaceholder list = null) {

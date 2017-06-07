55dab12341f23795aeaf2cbd01394d327be9fc70
Sagi Fogel <sagi.fogel@clicktale.com>
2017-06-07 22:42:39 +0300

    Addition of Sum unit-test

---
+++ Ramda.NET.Tests/Sum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ramda.NET.Tests
+{
+    [TestClass]
+    public class Sum
+    {
+        [TestMethod]
+        public void Sum_Adds_Together_The_Array_Of_Numbers_Supplied() {
+            Assert.AreEqual(R.Sum(new[] { 1, 2, 3, 4 }), 10);
+        }
+
+        [TestMethod]
+        public void Sum_Does_Not_Save_The_State_Of_The_Accumulator() {
+            Assert.AreEqual(R.Sum(new[] { 1, 2, 3, 4 }), 10);
+            Assert.AreEqual(R.Sum(new[] { 1 }), 1);
+            Assert.AreEqual(R.Sum(new[] { 5, 5, 5, 5, 5 }), 25);
+        }
+    }
+}

ed80433aa49334c35c4385f1e081f7ed7bafc3b1
Sagi Fogel <sagi.fogel@clicktale.com>
2017-07-05 23:42:40 +0300

    Addition of ZipObj unit-test

---
+++ Ramda.NET.Tests/ZipObj.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ramda.NET.Tests
+{
+    [TestClass]
+    public class ZipObj
+    {
+        [TestMethod]
+        public void ZipObj_Combines_An_Array_Of_Keys_With_An_Array_Of_Values_Into_A_Single_Object() {
+            DynamicAssert.AreEqual(R.ZipObj(new[] { "A", "B", "C" }, new[] { 1, 2, 3 }), new { A = 1, B = 2, C = 3 });
+        }
+
+        [TestMethod]
+        public void ZipObj_Ignores_Extra_Values() {
+            DynamicAssert.AreEqual(R.ZipObj(new[] { "A", "B", "C" }, new[] { 1, 2, 3, 4, 5, 6, 7 }), new { A = 1, B = 2, C = 3 });
+        }
+
+        [TestMethod]
+        public void ZipObj_Ignores_Extra_Keys() {
+            DynamicAssert.AreEqual(R.ZipObj(new[] { "A", "B", "C", "D", "E", "F" }, new[] { 1, 2, 3 }), new { A = 1, B = 2, C = 3 });
+        }
+
+        [TestMethod]
+        public void ZipObj_Last_One_In_Wins_When_There_Are_Duplicate_Keys() {
+            DynamicAssert.AreEqual(R.ZipObj(new[] { "A", "B", "C", "A" }, new object[] { 1, 2, 3, "LAST" }), new { A = "LAST", B = 2, C = 3 });
+        }
+    }
+}
